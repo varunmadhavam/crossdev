ee4a6cea6b9c37e799ef940b17f6d1a0699a9d3c
varun mohan <varunmadhavam@gmail.com>
2020-04-25 17:18:17 +0530

    basic setup for ARM

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 bins="bash  cat  ls  make  mkdir  rm  sed  sh"
-
+TOOL=""
+URL=""
 if [ "$#" -ne 2 ]; then
     echo "Usage ./install.sh ARCH TARGET_PATH"
     echo "ARCH can be AVR,ARM or XTENSA"
@@ -14,33 +15,40 @@ fi
 if [ -d "$2" ]; then
     CROSS_TOOLS="$2"/crosstools/"$1"/"$TOOL"/bin
     if [ "$1"=="ARM" ]; then
-	    TOOL="arm-none-eabi"
-        URL ="https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2"
+        TOOL="arm-none-eabi"
+        URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2"
+    
     elif [ "$1" == "AVR" ]; then
         echo "AVR choosen"
         exit 0
+    
     elif [ "$1" == "XTENSA" ]; then
         echo "Xtensa choosen"
         exit 0
+    
     else
         echo "Unsuported architecture " $1
         exit 1
     fi
-    mkdir -p "$2"/crosstools/"$2"
+   
+    mkdir -p "$2"/crosstools/"$1"
     if [ $? -ne 0 ]; then
         echo "Error while creating target directoy"
         exit 1
     fi
-    wget "$URL" -O tars/"$TOOL".tar.bz2
+    
+    wget $URL -O tars/"$TOOL".tar.bz2
     if [ $? -ne 0 ]; then
         echo "Error while downloading tools"
         exit 1
     fi
-    tar -xvf tars/"$TOOL".tar.bz2 -C "$2"/crosstools/"$1"
+    
+    tar --strip-components=1 -xvf  tars/"$TOOL".tar.bz2 -C "$2"/crosstools/"$1"
     if [ $? -ne 0 ]; then
         echo "Error while untarring tools"
         exit 1
     fi
+    
     for x in $bins; do allbins+="$(which $x) "; done
     for x in $(ls -ltr "$CROSS_TOOLS"/* | awk {'print $9'}); do allbins+="$x "; done
     for y in $(for x in $allbins; do ldd $x; done| grep "=>" | awk {'print $3'} | uniq); do libs+="$y ";done
