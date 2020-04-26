b908ba34245fb0064151506185103f2bf98c24fb
Varun Mohan <varunmadhavam@gmail.com>
2020-04-26 11:01:29 +0530

    Setting up libraries working

--- install.sh
+++ install.sh
@@ -5,24 +5,24 @@ TOOL=""
 URL=""
 if [ "$#" -ne 2 ]; then
     echo "Usage ./install.sh ARCH TARGET_PATH"
-    echo "ARCH can be AVR,ARM or XTENSA"
+    echo "ARCH can be avr,arm or xtensa"
     echo "TARGET_PATH is where the cross tools will be installed"
     echo "Tools will be installed at the path under TARGET_PATH/crosstools/ARCH"
-    echo "Eg: ./install.sh ARM /home/user/Tools"
+    echo "Eg: ./install.sh arm /home/user/Tools"
     exit 1
 fi
 
 if [ -d "$2" ]; then
-    CROSS_TOOLS="$2"/crosstools/"$1"/"$TOOL"/bin
-    if [ "$1"=="ARM" ]; then
+    rm -rf tars;mkdir tars
+    if [ "$1"=="arm" ]; then
         TOOL="arm-none-eabi"
         URL="https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2"
     
-    elif [ "$1" == "AVR" ]; then
+    elif [ "$1" == "avr" ]; then
         echo "AVR choosen"
         exit 0
     
-    elif [ "$1" == "XTENSA" ]; then
+    elif [ "$1" == "xtensa" ]; then
         echo "Xtensa choosen"
         exit 0
     
@@ -30,8 +30,8 @@ if [ -d "$2" ]; then
         echo "Unsuported architecture " $1
         exit 1
     fi
-   
-    mkdir -p "$2"/crosstools/"$1"
+
+    mkdir -p "$2"/crosstools/"$1"/crosscompiler/"$TOOL"
     if [ $? -ne 0 ]; then
         echo "Error while creating target directoy"
         exit 1
@@ -43,17 +43,19 @@ if [ -d "$2" ]; then
         exit 1
     fi
     
-    tar --strip-components=1 -xvf  tars/"$TOOL".tar.bz2 -C "$2"/crosstools/"$1"
+    tar --strip-components=1 -xvf  tars/"$TOOL".tar.bz2 -C "$2"/crosstools/"$1"/crosscompiler/"$TOOL"
     if [ $? -ne 0 ]; then
         echo "Error while untarring tools"
         exit 1
     fi
-    
+
+    CROSS_TOOLS="$2"/crosstools/"$1"/crosscompiler/"$TOOL"/bin
     for x in $bins; do allbins+="$(which $x) "; done
     for x in $(ls -ltr "$CROSS_TOOLS"/* | awk {'print $9'}); do allbins+="$x "; done
     for y in $(for x in $allbins; do ldd $x; done| grep "=>" | awk {'print $3'} | uniq); do libs+="$y ";done
     for y in $(for x in $allbins; do ldd $x; done | grep -v "=>" | grep -i "/lib" | awk {'print $1'}|uniq); do libs+="$y ";done
-    echo $libs
+    
+    for x in $libs; do mkdir -p /tmp/crosstools/arm/"`dirname $x`";cp $x /tmp/crosstools/arm/"`dirname $x`"/.;done
 else	
     echo "Path " $2 " doesnot exist"
     exit 1
